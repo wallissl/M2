ec6a3d394491d4204426bfda3aad203caaefc6f9
wallissl <wallis.sl@outlook.com>
2024-12-09 21:18:45 -0300

    S8A1

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,19 +1,15 @@
 const {Router}= require('express')
 
-/* const petsRoutes = require('./pets.routes'); // Importando o arquivo da rota de pets
-const vacinasRoutes = require('./vacinas.routes'); //Importando o arquivo da rota de vacinas
-const servicosRoutes = require('./servicos.routes');
-const pedidosRoutes = require('./pedidos.routes') */
-const responsaveisRoutes = require('./responsaveis.routes')
+
+const responsaveisRoutes = require('./responsaveis.routes');
+const cursosRoutes = require('./cursos.routes');
+
 
 const routes = new Router()
 
-routes.use('/responsaveis', responsaveisRoutes)
+routes.use('/responsaveis', responsaveisRoutes);
 
-// Utilizando as rotas importadas
-/* routes.use('/pets', petsRoutes); // o /pets é o endpoint
-routes.use('/vacinas', vacinasRoutes);
-routes.use('/servicos', servicosRoutes);
-routes.use('/pedidos', pedidosRoutes); */ // Criado para simular exercícios da semana 06
+routes.use('/cursos', cursosRoutes);
+
 
 module.exports = routes
