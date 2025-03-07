d174970675683e090400c4de6124cdc9d2f9f9f9
wallissl <wallis.sl@outlook.com>
2025-03-06 23:01:54 -0300

    m1s09

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,7 +3,7 @@ const responsaveisRoutes = require('./responsaveis.routes');
 const cursosRoutes = require('./cursos.routes');
 const usuariosRoutes = require('./usuarios.routes');
 const LoginController = require('../controllers/LoginController');
-const validaToken = require('../middlewares/validaToken');
+const auth = require('../middlewares/validaToken');
 
 
 const routes = new Router()
@@ -11,7 +11,7 @@ const routes = new Router()
 routes.use('/usuarios', usuariosRoutes);
 routes.post('/login', LoginController.login);
 
-routes.use(validaToken) // Tudo que estiver abaixo só será acessado se o usuário tiver o token.
+routes.use(auth) // Tudo que estiver abaixo só será acessado se o usuário tiver o token.
 routes.use('/responsaveis', responsaveisRoutes);
 routes.use('/cursos', cursosRoutes);
 
