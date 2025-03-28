b302631ce08a5553d2b3c9f65faa2a6eecdf661c
Aluno LAB <aluno.lab@ides.org.br>
2025-03-28 17:20:04 -0300

    configurações iniciais do swagger

--- src/routes/cursos.routes.js
+++ src/routes/cursos.routes.js
@@ -5,16 +5,61 @@ const verificarPermissao = require('../middlewares/verificarPermissao');
 
 const cursosRoutes = new Router();
 
-cursosRoutes.post('/',verificarPermissao(['criarCurso']), cursoController.criar); // 'Criar curso';
+cursosRoutes.post('/',verificarPermissao(['criarCurso']), cursoController.criar
 
-cursosRoutes.get('/', verificarPermissao(['listarCurso']),cursoController.buscarTodos); // 'Listar curso';
+    /*
+    
+    #swagger.tags = ['Cursos'],
+    #swagger.description = 'Endpoint para criar um curso',
+    #swagger.parameters['criarCurso'] = {
+      in: 'body',
+      description: 'Dados do curso',
+      required: true,
+      schema: { 
+        $nome: "Curso de JavaScript",
+        $duracao: 40,
+       
+      }
+    },   
+    
+    */
 
-cursosRoutes.get('/parametro', cursoController.listarPorParametro);
+); // 'Criar curso';
 
-cursosRoutes.delete('/:id', verificarPermissao(['listarCurso', 'deletarCurso']), cursoController.deletar);
+cursosRoutes.get('/', verificarPermissao(['listarCurso']),cursoController.buscarTodos
 
-cursosRoutes.put('/:id', cursoController.atualizar);
+    /*
+        #swagger.tags = ['Cursos'],
+    */
 
-cursosRoutes.get('/:id', cursoController.buscarUm);
+); // 'Listar curso';
+
+cursosRoutes.get('/parametro', cursoController.listarPorParametro
+
+     /*
+        #swagger.tags = ['Cursos'],
+    */
+);
+
+cursosRoutes.delete('/:id', verificarPermissao(['listarCurso', 'deletarCurso']), cursoController.deletar
+
+ /*
+        #swagger.tags = ['Cursos'],
+    */
+);
+
+cursosRoutes.put('/:id', cursoController.atualizar
+    
+    /*
+        #swagger.tags = ['Cursos'],
+    */
+);
+
+cursosRoutes.get('/:id', cursoController.buscarUm
+
+    /*
+        #swagger.tags = ['Cursos'],
+    */
+);
 
 module.exports = cursosRoutes
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -3,6 +3,24 @@ const UsuarioController = require('../controllers/UsuarioController')
 
 const usuariosRoutes = new Router()
 
-usuariosRoutes.post("/", UsuarioController.criarConta);
+usuariosRoutes.post("/", UsuarioController.criarConta
+
+    /*
+    
+    #swagger.tags = ['Usuarios'],
+    #swagger.description = 'Endpoint para criar um usuário',
+    #swagger.parameters['criarUsuario'] = {
+      in: 'body',
+      description: 'Dados do usuário',
+      required: true,
+      schema: { 
+        $nome: "Usuario Teste",
+        $email: "teste@gmail.com",
+        $password: "teste123",       
+      }
+    },   
+    
+    */
+);
 
 module.exports = usuariosRoutes
